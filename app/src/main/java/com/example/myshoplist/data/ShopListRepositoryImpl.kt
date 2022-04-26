3369ec9fce1ab64b7d7c1f6c10b35989ba08de56
auelbai <auelbai12@mail.ru>
2022-04-26 07:43:44 +0600

    добавлены интеракторы и репозитория в домеин слое, сделана реализация юзкейсов в дата слое, добавлена реализация вью модели

---
+++ app/src/main/java/com/example/myshoplist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,51 @@
+package com.example.myshoplist.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.myshoplist.domain.ShopItem
+import com.example.myshoplist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private var shopList = mutableListOf<ShopItem>()
+    private var shopItemId = 0
+
+    init {
+        for (i in 0 until 10) {
+            addShopItem(ShopItem("name $i", i, true))
+        }
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = shopItemId++
+        }
+        shopList.add(shopItem)
+        updateShopList()
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+        updateShopList()
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldShopItem = getShopItem(shopItem.id)
+        deleteShopItem(oldShopItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shopList.find { it.id == id } ?: throw RuntimeException("ShopItemId $id not found")
+    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateShopList() {
+        shopListLD.value = shopList.toList()
+    }
+}
---
+++ app/src/main/java/com/example/myshoplist/presentation/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.myshoplist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.myshoplist.data.ShopListRepositoryImpl
+import com.example.myshoplist.domain.*
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newShopItem)
+    }
+
+}
