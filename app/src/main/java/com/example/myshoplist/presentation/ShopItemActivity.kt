8ab4a288e1536ae9d8c115b934f2962eb698f442
auelbai <auelbai12@mail.ru>
2022-05-02 19:39:27 +0600

    создали viewModel для активити Айтема

---
+++ app/src/main/java/com/example/myshoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myshoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myshoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
